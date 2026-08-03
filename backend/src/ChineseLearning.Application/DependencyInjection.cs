9ef7cf2688e1c3d376fb9000971776ef1b1ce061
Nguyễn Thanh Vũ <nthanhvu1411@gmail.com>
2026-08-03 12:33:17 +0700

    refactor(auth): register application services

--- backend/src/ChineseLearning.Application/DependencyInjection.cs
+++ backend/src/ChineseLearning.Application/DependencyInjection.cs
@@ -1,8 +1,14 @@
+using ChineseLearning.Application.Abstractions.Authentication;
+using ChineseLearning.Application.Features.Authentication.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ChineseLearning.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services) => services;
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthService, AuthService>();
+        return services;
+    }
 }
